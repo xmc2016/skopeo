c07f20982c26acbcd46013cef6799bd142a96cc7
Christoph Blecker <cblecker@redhat.com>
2023-02-23 09:06:16 -0800

    Fix formatting of inspect examples
    Signed-off-by: Christoph Blecker <cblecker@redhat.com>

--- cmd/skopeo/inspect.go
+++ cmd/skopeo/inspect.go
@@ -51,8 +51,8 @@ See skopeo(1) section "IMAGE NAMES" for the expected format
 `, strings.Join(transports.ListNames(), ", ")),
 		RunE: commandAction(opts.run),
 		Example: `skopeo inspect docker://registry.fedoraproject.org/fedora
-  skopeo inspect --config docker://docker.io/alpine
-  skopeo inspect  --format "Name: {{.Name}} Digest: {{.Digest}}" docker://registry.access.redhat.com/ubi8`,
+skopeo inspect --config docker://docker.io/alpine
+skopeo inspect --format "Name: {{.Name}} Digest: {{.Digest}}" docker://registry.access.redhat.com/ubi8`,
 		ValidArgsFunction: autocompleteSupportedTransports,
 	}
 	adjustUsage(cmd)
